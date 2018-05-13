772da9bcd78dc5fd36e6e9d573b2a925e6032d83
auggod <hello@auggod.tech>
2018-05-13 10:39:11 +0200

    rename function

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var leftPad = require('left-pad')
 
-module.exports = Clock
+module.exports = secondsToMinutesAndSeconds
 
 /*
  * Convert seconds in digital format
  * @example
  * returns 01:29
- * clock(89);
+ * secondsToMinutesAndSeconds(89);
  * @param {Number} duration The total duration in seconds
  * @returns {String} Returns digital time in minutes and seconds
  */
 
-function Clock (duration) {
+function secondsToMinutesAndSeconds (duration) {
   var d = parseFloat(duration, 10)
 
   if (d < 0 || isNaN(d)) return '0:00'
